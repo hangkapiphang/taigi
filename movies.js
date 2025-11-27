b94d71c0ac15b00c62a49a0f33374a160406642d
hangkapiphang <hangkapiphang@gmail.com>
2025-11-27 15:51:10 +0800

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,13 @@
+// This file holds your movie database
+const movieLibrary = {
+    // ID : { Video Link, Subtitle Link }
+    "demo": {
+        video: "https://youtu.be/Jxc_0veNt2U?si=IaWcVAexhPpRWG4b",
+        sub: "https://raw.githubusercontent.com/hangkapiphang/taigi/refs/heads/main/subtitles/POJ.srt"
+    },
+    "movie2": {
+        video: "https://youtu.be/ANOTHER_LINK",
+        sub: "https://link.to/another.srt"
+    }
+    // Add more movies here...
+};
