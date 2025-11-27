d8d611968a1f0ea954e8b8e976695cca3adb7f0a
hangkapiphang <hangkapiphang@gmail.com>
2025-11-27 16:22:08 +0800

    Update movies.js

--- movies.js
+++ movies.js
@@ -5,6 +5,10 @@ const movieLibrary = {
         video: "https://youtu.be/Jxc_0veNt2U?si=IaWcVAexhPpRWG4b",
         sub: "https://raw.githubusercontent.com/hangkapiphang/taigi/refs/heads/main/subtitles/POJ.srt"
     },
+        "BraveTaiwanese": {
+        video: "https://youtu.be/Fy_Lj_wEiLc",
+        sub: "https://raw.githubusercontent.com/hangkapiphang/taigi/refs/heads/main/subtitles/BraveTaiwanese.srt"
+    },
     "movie2": {
         video: "https://youtu.be/ANOTHER_LINK",
         sub: "https://link.to/another.srt"
